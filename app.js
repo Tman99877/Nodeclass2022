ea406e2766ff0fde4f75d505c55ae92664503a50
Tman99877 <wallacetyrese8@gmail.com>
2022-10-15 20:37:36 +0000

    up3

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 var express = require('express');
+var mongoose = require('mongoose')
 var app = express();
 
 app.use('/static', express.static ("public"));
+app.use(express.urlencoded({extended: true}))
 app.set("view engine","ejs");
 
-app.get('/hello', function(req,res){
-    res.send('Hello World');
-})
+const mongodb = 'mongodb+srv://wallace_tyrese:cz69Jl8MzqgTRr8w@cluster0.bq0gq1u.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(mongodb);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, "MongoDB error connection"))
 
 app.get('/', function(req,res){
     res.render('todo.ejs');
+})
+
+app.post('/',(req,res)=>{
+    console.log(req.body.content)
 })
 
 app.listen(3000,function(){
